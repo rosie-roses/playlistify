90a93d5809d9418e9fc2289a33a49fa152632cf7
Amarasinghe Saparamadu <saparaamar1@ecs.vuw.ac.nz>
2022-12-17 03:10:54 +1300

    Fixed connection to MongoDB.

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const connectDB = async() => {
+const connectDB = () => {
     try {
         // MongoDB connection string.
-        const con = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        mongoose.set("strictQuery", false);
+        mongoose.connect(process.env.MONGO_URI, () => {
+            console.log(`Connected to MongoDB.`);
         });
-        console.log(`MongoDB connect:${con.connection.host}`);
     } catch (err) {
         console.log(err);
         process.exit(1);
