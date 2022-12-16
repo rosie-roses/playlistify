52faee1d21633c6152651440c2034c2db1459690
Amarasinghe Saparamadu <saparaamar1@ecs.vuw.ac.nz>
2022-12-16 21:34:54 +1300

    Added skeleton register form.

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", init);
+function init() {
+    console.log("In init()")
+}
+// document.querySelector('#register_user').addEventListener("click", () => {console.log("button clicked")})
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
-dotenv.config({ path: 'config.env' });
-const PORT = process.env.PORT || 8080;
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require('path');
+
+const app = express();
+
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
 
 // Log requests.
 app.use(morgan("dev"));
@@ -13,18 +17,20 @@ app.use(morgan("dev"));
 // Parse requests to body-parser.
 app.use(bodyparser.urlencoded({ extended: true }));
 
+// Set view engine.
+app.set("view engine", "ejs");
+
 // Load assets.
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
 
-// Set view engine.
-app.set("view engine", "ejs");
-
-app.use(express.json());
-
 app.get("/", (req, res) => {
     res.render('index');
-})
+});
+
+app.get("/register", (req, res) => {
+    res.render('register');
+});
 
 app.listen(3000, ()=> { console.log(`Server is running on http://localhost:${PORT}`) });
