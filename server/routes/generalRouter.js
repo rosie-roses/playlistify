de63abd66b8415d41960de8b9dc8aefab1bfe329
Amarasinghe Saparamadu <saparaamar1@ecs.vuw.ac.nz>
2022-12-21 23:28:52 +1300

    Added a middleware for checking the current user.

--- server/routes/generalRouter.js
+++ server/routes/generalRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const generalRouter = express.Router();
 const controller = require('../controller/controller');
-const { requireAuth } = require('../middleware/authMiddleware');
+const { requireAuth, checkUser } = require('../middleware/authMiddleware');
 
+generalRouter.get('*', checkUser); // Apply checkUser middleware to every single GET request.
 generalRouter.get('/', controller.index);
 generalRouter.get('/profile', requireAuth , controller.profile);
 
